53b31cebc58897eaffac917ac109c15447c58c5e
dhruv9596 <dhruvlodaliya9596@gmail.com>
2023-05-12 13:49:00 +0530

    Initial Commit

---
+++ Frontend/src/pages/Root.js
@@ -0,0 +1,34 @@
+import { Outlet, useLoaderData , useSubmit} from 'react-router-dom';
+import { useEffect } from 'react';
+import MainNavigation from '../components/MainNavigation';
+
+function RootLayout() {
+  // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+  useEffect(() => {
+    if(!token) {
+      return ;
+    }
+    if(token === 'EXPIRED') {
+      submit( null , {action : '/logout' , method : 'post'});
+    }
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration);
+    setTimeOut(() => {
+      submit( null , {action : '/logout' , method : 'post'});
+    } , tokenDuration );
+  }, [token ,  submit]);
+  
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
